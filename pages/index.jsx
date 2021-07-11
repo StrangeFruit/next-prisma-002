86ba5041630283d3e4278665267ba6ca70841a59
Shalom JS <shalom.devop@gmail.com>
2021-07-11 09:55:55 -0400

    updated prisma

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import React, {useState} from 'react'
-import {PrismaClient} from '@prisma/client'
+import prisma from '../lib/prisma';
 
 
-const prisma = new PrismaClient();
+
 
 export async function getServerSideProps(context) {
   const message = await prisma.message.findMany();
